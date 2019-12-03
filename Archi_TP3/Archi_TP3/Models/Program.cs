7251939e8dbf4843b8d320ec134ebc5e8257a8bd
Lena Albert <lenalbert@hotmail.fr>
2019-12-03 16:54:13 -0500

    Program.cs

---
+++ Archi_TP3/Archi_TP3/Models/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Archi_TP3.Models
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new Context())
+            {
+
+                var user = new User() { login="azerty", password="azerty" };
+                ctx.Users.Add(user);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
