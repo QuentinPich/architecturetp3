f3bc44314ad15d0efc6bd44f266d82427edefec3
Sophie Thongrasamy <sophie.thongrasamy@gmail.com>
2019-12-03 15:26:16 -0500

    Classe emplyee maj

--- Archi_TP3/Archi_TP3/Models/Employee.cs
+++ Archi_TP3/Archi_TP3/Models/Employee.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Archi_TP3.Models
 {
     public class Employee
     {
-        public int IdEmployee { get; set; }
+        public int EmployeeID { get; set; }
+        [ForeignKey("User")] public int UserID { get; set; }
         public string LastNameEmployee { get; set; }
         public string FirstNameEmployee { get; set; }
         public string EmailEmployee { get; set; }
         public string AddressEmployee { get; set; }
         public int TelephoneEmployee { get; set; }
         public DateTime BirthDateEmployee { get; set; }
-        public int HourlyCostEmployee { get; set; }
+        public DateTime HiringDateEmployee { get; set; }
+
+        public virtual User User { get; set; }
+
     }
 }
