f0baaa6fcbd49e3db16e896c9e0ca4a7f1b504c7
Quentin Pichollet <quentin.pichollet@ensiie.fr>
2019-12-07 18:32:19 -0500

    fusion

---
+++ Archi_TP3/Archi_TP3/Migration/Clinic/201912051927352_InitialCreate1.cs
@@ -0,0 +1,50 @@
+﻿namespace Archi_TP3.Migration.Clinic
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Patients", "User_UserID", "dbo.Users");
+            DropForeignKey("dbo.Equipments", "RoomID", "dbo.Rooms");
+            DropIndex("dbo.Patients", new[] { "User_UserID" });
+            DropIndex("dbo.Equipments", new[] { "RoomID" });
+            AddColumn("dbo.Rooms", "Equipment_EquipmentID", c => c.Int());
+            AddColumn("dbo.Equipments", "Room_RoomID", c => c.Int());
+            AlterColumn("dbo.Appointments", "AppointmentDate", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Employees", "BirthDateEmployee", c => c.DateTime());
+            AlterColumn("dbo.Employees", "HiringDateEmployee", c => c.DateTime());
+            AlterColumn("dbo.Users", "rights", c => c.String());
+            AlterColumn("dbo.Patients", "BirthDatePatient", c => c.DateTime());
+            AlterColumn("dbo.PatientRecords", "PatientHistory", c => c.String());
+            CreateIndex("dbo.Rooms", "Equipment_EquipmentID");
+            CreateIndex("dbo.Equipments", "Room_RoomID");
+            AddForeignKey("dbo.Rooms", "Equipment_EquipmentID", "dbo.Equipments", "EquipmentID");
+            AddForeignKey("dbo.Equipments", "Room_RoomID", "dbo.Rooms", "RoomID");
+            DropColumn("dbo.Patients", "User_UserID");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Patients", "User_UserID", c => c.Int());
+            DropForeignKey("dbo.Equipments", "Room_RoomID", "dbo.Rooms");
+            DropForeignKey("dbo.Rooms", "Equipment_EquipmentID", "dbo.Equipments");
+            DropIndex("dbo.Equipments", new[] { "Room_RoomID" });
+            DropIndex("dbo.Rooms", new[] { "Equipment_EquipmentID" });
+            AlterColumn("dbo.PatientRecords", "PatientHistory", c => c.String(unicode: false, storeType: "text"));
+            AlterColumn("dbo.Patients", "BirthDatePatient", c => c.DateTime(nullable: false, storeType: "date"));
+            AlterColumn("dbo.Users", "rights", c => c.Int(nullable: false));
+            AlterColumn("dbo.Employees", "HiringDateEmployee", c => c.DateTime(nullable: false, storeType: "date"));
+            AlterColumn("dbo.Employees", "BirthDateEmployee", c => c.DateTime(nullable: false, storeType: "date"));
+            AlterColumn("dbo.Appointments", "AppointmentDate", c => c.DateTime(nullable: false, storeType: "date"));
+            DropColumn("dbo.Equipments", "Room_RoomID");
+            DropColumn("dbo.Rooms", "Equipment_EquipmentID");
+            CreateIndex("dbo.Equipments", "RoomID");
+            CreateIndex("dbo.Patients", "User_UserID");
+            AddForeignKey("dbo.Equipments", "RoomID", "dbo.Rooms", "RoomID", cascadeDelete: true);
+            AddForeignKey("dbo.Patients", "User_UserID", "dbo.Users", "UserID");
+        }
+    }
+}
