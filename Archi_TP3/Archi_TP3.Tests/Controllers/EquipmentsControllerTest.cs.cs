3f66f5ae22037b3394ef02f63fc2379d78bc7395
Quentin Pichollet <quentin.pichollet@ensiie.fr>
2019-12-07 18:29:32 -0500

    tests unitaires 22/42 ok

---
+++ Archi_TP3/Archi_TP3.Tests/Controllers/EquipmentsControllerTest.cs.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Archi_TP3;
+using Archi_TP3.Controllers;
+
+namespace Archi_TP3.Tests.Controllers
+{
+    [TestClass]
+    public class EquipmentsControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            EquipmentsController controller = new EquipmentsController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            EquipmentsController controller = new EquipmentsController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        public void Edit(int? id)
+        {
+            // Arrange
+            EquipmentsController controller = new EquipmentsController();
+
+            // Act
+            ViewResult result = controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        public void Delete(int? id)
+        {
+            // Arrange
+            EquipmentsController controller = new EquipmentsController();
+
+            // Act
+            ViewResult result = controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ Archi_TP3/Archi_TP3.Tests/Controllers/ReasonsControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Archi_TP3;
+using Archi_TP3.Controllers;
+
+namespace Archi_TP3.Tests.Controllers
+{
+    [TestClass]
+    public class ReasonsControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            ReasonsController controller = new ReasonsController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            ReasonsController controller = new ReasonsController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        public void Edit(int? id)
+        {
+            // Arrange
+            ReasonsController controller = new ReasonsController();
+
+            // Act
+            ViewResult result = controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        public void Delete(int? id)
+        {
+            // Arrange
+            ReasonsController controller = new ReasonsController();
+
+            // Act
+            ViewResult result = controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
